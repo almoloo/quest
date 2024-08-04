890f8161f9d0b9335f72543c43bbc088ac631ca3
Ali Mousavi <amousavig@icloud.com>
2024-08-04 06:10:44 +0330

    Finished footer.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,45 @@
+'use client';
+
+import { ExperimentTwoTone, GithubOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import Link from 'next/link';
+
 const Footer = () => {
 	return (
-		<footer className="padding border-t">
-			Footer <div></div>
+		<footer className="padding flex justify-between items-center bg-slate-100">
+			<div>
+				<ExperimentTwoTone />
+				<small className="ml-2">
+					Cooked up by{' '}
+					<Link
+						href="https://github.com/almoloo"
+						target="_blank"
+						passHref
+						className="text-sky-500"
+					>
+						almoloo
+					</Link>{' '}
+					&{' '}
+					<Link
+						href="https://github.com/Hossein-79"
+						target="_blank"
+						passHref
+						className="text-sky-500"
+					>
+						Hossein-79
+					</Link>
+				</small>
+			</div>
+			<Link
+				href="https://github.com/almoloo/quest"
+				passHref
+			>
+				<Button
+					type="link"
+					icon={<GithubOutlined />}
+					size="large"
+				/>
+			</Link>
 		</footer>
 	);
 };
