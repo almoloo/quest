a95561fa15d5c5526ca8d96c13a2df5f9ab63449
Ali Mousavi <amousavig@icloud.com>
2024-08-25 01:49:55 +0330

    Finished homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import {
+	EditTwoTone,
+	SafetyCertificateTwoTone,
+	WalletTwoTone,
+} from '@ant-design/icons';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -24,46 +29,82 @@ export default function Home() {
 	return (
 		<>
 			{/* ----- HERO ----- */}
-			<section>
-				<h2>Create & Share Your Achievements as NFTs</h2>
-				<span>
-					Turn your accomplishments into unique digital badges and
-					send them as NFTs to recognize others.
-				</span>
-			</section>
-			{/* ----- FEATURES ----- */}
-			<section>
-				<h3>Why Choose Quest?</h3>
-				<div>
-					{/* ITEM */}
-					<div>
-						<h4>Customizable Templates</h4>
-						<span>
-							Choose from a variety of professionally designed
-							templates to create your unique achievement badges.
-						</span>
-					</div>
-					{/* ITEM */}
-					<div>
-						<h4>Easy Wallet Integration</h4>
-						<span>
-							Connect your digital wallet seamlessly and start
-							creating and sending badges in minutes.
-						</span>
-					</div>
-					{/* ITEM */}
-					<div>
-						<h4>Secure and Decentralized</h4>
-						<span>
-							Powered by blockchain, ensuring your badges are
-							secure, authentic, and yours forever.
-						</span>
-					</div>
+			<section className="flex flex-col-reverse lg:grid lg:grid-cols-7 justify-center items-center">
+				<div className="lg:col-span-3 lg:col-start-2 p-10 lg:p-0">
+					<h2 className="font-black text-3xl leading-relaxed mb-2">
+						Transform Your Achievements into Digital Badges
+					</h2>
+					<span className="font-serif text-lg text-neutral-600">
+						Create, customize, and share achievement badges as NFTs
+						with Quest.
+					</span>
+				</div>
+				<div className="lg:col-span-2">
+					<Image
+						src={'/badge.png'}
+						alt="Badge"
+						width={400}
+						height={400}
+						className="drop-shadow-2xl"
+					/>
 				</div>
 			</section>
-			{/* ----- HOW ----- */}
-			<section>
-				<h3>How It Works</h3>
+			{/* ----- ABOUT ----- */}
+			<section className="lg:grid lg:grid-cols-7 justify-center p-10 lg:p-0 mt-5 lg:mt-10">
+				<div className="lg:col-span-5 lg:col-start-2">
+					<h3 className="font-bold text-xl mb-2">About Quest</h3>
+					<p className="text-neutral-800">
+						Quest is a platform that allows you to immortalize your
+						achievements by turning them into unique digital badges.
+						Using blockchain technology, these badges can be
+						customized, minted as NFTs, and shared with others,
+						making your success story a part of the digital world.
+					</p>
+				</div>
+			</section>
+			{/* ----- FEATURES ----- */}
+			<section className="lg:grid lg:grid-cols-7 justify-center p-10 lg:p-0 mt-5 lg:mt-10 mb-10">
+				<div className="lg:col-span-5 lg:col-start-2">
+					<h3 className="font-bold text-xl mb-2">
+						Why Choose Quest?
+					</h3>
+					<div className="flex flex-col lg:grid lg:grid-cols-3 gap-5 mt-5">
+						{/* ITEM */}
+						<div>
+							<h4 className="font-bold mb-1">
+								<EditTwoTone className="mr-2" />
+								Customizable Templates
+							</h4>
+							<span className="text-sm text-neutral-600">
+								Choose from a variety of professionally designed
+								templates to create your unique achievement
+								badges.
+							</span>
+						</div>
+						{/* ITEM */}
+						<div>
+							<h4 className="font-bold mb-1">
+								<WalletTwoTone className="mr-2" />
+								Easy Wallet Integration
+							</h4>
+							<span className="text-sm text-neutral-600">
+								Connect your digital wallet seamlessly and start
+								creating and sending badges in minutes.
+							</span>
+						</div>
+						{/* ITEM */}
+						<div>
+							<h4 className="font-bold mb-1">
+								<SafetyCertificateTwoTone className="mr-2" />
+								Secure and Decentralized
+							</h4>
+							<span className="text-sm text-neutral-600">
+								Powered by blockchain, ensuring your badges are
+								secure, authentic, and yours forever.
+							</span>
+						</div>
+					</div>
+				</div>
 			</section>
 		</>
 	);
